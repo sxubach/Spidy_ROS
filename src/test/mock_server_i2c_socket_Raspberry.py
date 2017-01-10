b5249b31ccaf5de66cdc86e86b2f01437233b921
sxubach <sxubach@gmail.com>
2017-01-10 19:37:30 +0100

    python server accepts multyclient, updated from the mock server. Added a security copy in case this modifications dont work in the real raspberry

--- src/test/mock_server_i2c_socket_Raspberry.py
+++ src/test/mock_server_i2c_socket_Raspberry.py
@@ -23,83 +23,12 @@ s.bind((HOST, PORT))
 print 'Socket bind succesful'
 
 s.listen(10) #number of tolerable unaccepted connections
-'''
-c, addr = s.accept()
-print 'Connected with ' + addr[0] + ':' + str(addr[1])
-## Socket configuration end
-
-
-## Main loop
-print ' '
-while(1):
-	print 'Setting values ' + str(count) + ':'
-	count = count + 1
-
-	try:	
-		#print 'Ultrasound start'
-		distance_U = 42
-		#print 'distance_U =' + str(distance_U)
-
-		#print 'Accelerometer'
-		accel_X = 1
-		accel_Y = 2
-		accel_Z = 3
-
-		#print 'Gyroscope'
-		gyro_X = 3
-		gyro_Y = 2
-		gyro_Z = 1
-
-		print 'distance_U =' + str(distance_U)
-		print 'accel_X =' + str(accel_X)
-		print 'accel_Y =' + str(accel_Y)
-		print 'accel_Z =' + str(accel_Z)
-		print 'gyro_X =' + str(gyro_X)
-		print 'gyro_Y =' + str(gyro_Y)
-		print 'gyro_Z =' + str(gyro_Z)
-		print " "
-		
-		state = ord(c.recv(1024))-10
-		c.send(str(distance_U))
-		c.recv(1024)
-		c.send(str(accel_X))
-		c.recv(1024)
-		c.send(str(accel_Y))
-		c.recv(1024)
-		c.send(str(accel_Z))
-		c.recv(1024)
-		c.send(str(gyro_X))
-		c.recv(1024)
-		c.send(str(gyro_Y))
-		c.recv(1024)
-		c.send(str(gyro_Z))
-		c.recv(1024)
-
-		for x in range(0, 12):
-			c.send("ack.")
-			pwm_value = ord(c.recv(1024))-10
-			print 'pwm[' + str(x) + '] = '+ str(pwm_value)
-			
-
-	except:
-		print 'Connection lost'
-		c.close()
-		s.listen(10)
-		c, addr = s.accept()
-		print 'after accept'
-	
-	print " "
-	print " "
-
-c.close()
-s.close()
-'''
 
 def client_thread(c):
         print OKBLUE + 'Client connected' + ENDC
         count = 0
         while(1):
-	        print OKBLUE + 'Trhead count = ' + str(count) + ENDC
+	        print OKBLUE + 'Trhead execution count = ' + str(count) + ENDC
                 count = count +1
 
 		distance_U = 42
